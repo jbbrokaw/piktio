67412c786381e4af718a5bff339bba215768f688
Jason Brokaw <jbrokaw@gmail.com>
2014-12-09 16:47:44 -0800

    Adds vagrant setup for db.

--- vagrantpg/Vagrant-setup/bootstrap.sh
+++ vagrantpg/Vagrant-setup/bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -e
 
 # Edit the following to change the name of the database user that will be created:
-APP_DB_USER=myapp
-APP_DB_PASS=dbpass
+APP_DB_USER=piktio
+APP_DB_PASS=12345
 
 # Edit the following to change the name of the database that is created (defaults to the user name)
 APP_DB_NAME=$APP_DB_USER
