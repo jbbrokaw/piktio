516b09961b815bf4f8e51f543343efffba57626a
jbbrokaw <jbrokaw@gmail.com>
2014-12-23 22:59:48 -0700

    Adds working follow buttons to completed game view page

--- piktio/serializers.py
+++ piktio/serializers.py
@@ -1,42 +1,47 @@
-def show_completed_game(gm):
+def show_completed_game(gm, request):
     return {'id': gm.id,
 
             'subject': gm.subject.subject,
 
-            'subject_author': gm.subject.author.display_name,
+            'subject_author': author(gm.subject.author, request),
 
             'predicate': gm.predicate.predicate,
 
-            'predicate_author': gm.predicate.author.display_name,
+            'predicate_author': author(gm.predicate.author, request),
 
             'first_drawing': gm.first_drawing.identifier,
 
-            'first_drawing_author':
-            gm.first_drawing.author.display_name,
+            'first_drawing_author': author(gm.first_drawing.author, request),
 
             'first_description': gm.first_description.description,
 
             'first_description_author':
-            gm.first_description.author.display_name,
+            author(gm.first_description.author, request),
 
             'second_drawing': gm.second_drawing.identifier,
 
             'second_drawing_author':
-            gm.second_drawing.author.display_name,
+            author(gm.second_drawing.author, request),
 
             'second_description': gm.second_description.description,
 
             'second_description_author':
-            gm.second_description.author.display_name,
+            author(gm.second_description.author, request),
 
             'time_completed': gm.time_completed.strftime("%m/%d/%y %H:%M")
             }
 
 
-def game_list(games):
+def game_list(games, request):
     if games:
-        lst = [show_completed_game(games[0])]
+        lst = [show_completed_game(games[0], request)]
     else:
         lst = []
     lst.extend([{'id': gm.id} for gm in games[1:]])
     return lst
+
+
+def author(author, request):
+    return {'id': author.id,
+            'display_name': author.display_name,
+            'followed': (author in request.user.followees)}
